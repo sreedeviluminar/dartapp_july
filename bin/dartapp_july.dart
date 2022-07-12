46242752900e342e0c65444d635d94703a55ff2e
Sreedevi <sreedevi@luminartechnolab.com>
2022-07-12 10:24:06 +0530

    first commit

---
+++ bin/dartapp_july.dart
@@ -0,0 +1,5 @@
+import 'package:dartapp_july/dartapp_july.dart' as dartapp_july;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartapp_july.calculate()}!');
+}
---
+++ test/dartapp_july_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartapp_july/dartapp_july.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
